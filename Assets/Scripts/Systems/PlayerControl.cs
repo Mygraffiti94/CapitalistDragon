45624eb9b0832f0e4354c406dba8fe779619afd2
Mike <mygraffiti94@gmail.com>
2021-07-21 20:36:07 -0400

    Adding Movement to Character
    Adding movement animations to Knight character.

---
+++ Assets/Scripts/Systems/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	public float moveSpeed;
+	
+	// Processing variables
+	private Animator	moveAnimator;
+	private	float		xInput;
+	private float		yInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		xInput = Input.GetAxisRaw("Horizontal");
+		yInput = Input.GetAxisRaw("Vertical");
+
+        if(xInput > 0f)
+			transform.Translate(new Vector3(xInput * moveSpeed * Time.deltaTime, 0f, 0f));
+		else if (Input.GetAxisRaw("Horizontal") < 0f)
+			transform.Translate(new Vector3(xInput * moveSpeed * Time.deltaTime, 0f, 0f));
+		if (Input.GetAxisRaw("Vertical") > 0f)
+			transform.Translate(new Vector3(0f, yInput * moveSpeed * Time.deltaTime, 0f));
+		else if (Input.GetAxisRaw("Vertical") < 0f)
+			transform.Translate(new Vector3(0f, yInput * moveSpeed * Time.deltaTime, 0f));
+
+		moveAnimator.SetFloat("MoveX", xInput);
+		moveAnimator.SetFloat("MoveY", yInput);
+		if(xInput > 0.1 || xInput < -0.1
+		|| yInput > 0.1 || yInput < -0.1)
+		{
+			moveAnimator.SetFloat("LastMoveX", xInput);
+			moveAnimator.SetFloat("LastMoveY", yInput);
+		}
+	}
+}
